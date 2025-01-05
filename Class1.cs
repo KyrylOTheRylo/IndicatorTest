9f99fb1fbac8ea3962924c867f644627e49a4a25
KyryloTheRylo <kiril.mosiychuk@gmail.com>
2025-01-06 01:27:58 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace ATAS.Indicators.Technical
+{
+
+    using ATAS.Indicators;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class IndicatorTests : Indicator
+    {
+        private int _period = 10;
+
+        [Display(GroupName = "Variables", Name = "Period", Order = 10)]
+        public int Period { 
+            get { return _period; } 
+            set { _period = value;
+                RecalculateValues();
+            } 
+        }
+
+        protected override void OnCalculate(int bar, decimal value)
+        {
+            var start = Math.Max(0, bar - Period + 1);
+            var count = Math.Min(bar + 1, Period);
+            var max = (decimal)SourceDataSeries[start];
+            for (var i = start + 1; i < start + count; i++)
+            {
+                max = Math.Max(max, (decimal)SourceDataSeries[i]);
+            }
+
+            this[bar] = max;
+        }
+    }
+}
