34c3634781350e88fd6f1cbd90f4acaf4697d0f9
KyryloTheRylo <kiril.mosiychuk@gmail.com>
2025-01-18 03:47:49 +0200

    FEATURE: Get custom drawing on Render.

--- Class1.cs
+++ Class1.cs
@@ -3,12 +3,13 @@
 
     using ATAS.Indicators;
     using ATAS.Indicators.Drawing;
+    using OFT.Rendering.Context;
     using OFT.Rendering.Settings;
+    using OFT.Rendering.Tools;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.Drawing;
-    using System.Windows.Media;
 
     public enum ClusterT
     {
@@ -26,6 +27,10 @@
 
         public IndicatorTests()
         {
+            EnableCustomDrawing = true;
+
+            //Subscribing only to drawing on final layout
+            SubscribeToDrawingEvents(DrawingLayouts.Final);
             LineSeries.Add(new LineSeries("Down")
             {
                 Color = System.Windows.Media.Colors.Orange,
@@ -34,6 +39,30 @@
                 Width = 1
             });
 
+        }
+        protected override void OnRender(RenderContext context, DrawingLayouts layout)
+        {
+            // creating pen, width 4px
+            var pen = new RenderPen(Color.BlueViolet, 4);
+
+            //drawing horizontal line
+            context.DrawLine(pen, 0, MouseLocationInfo.LastPosition.Y, ChartArea.Width, MouseLocationInfo.LastPosition.Y);
+
+            //drawing vertical line
+            context.DrawLine(pen, MouseLocationInfo.LastPosition.X, 0, MouseLocationInfo.LastPosition.X, ChartArea.Height);
+
+            var candle = GetCandle(MouseLocationInfo.BarBelowMouse);
+
+            if (candle != null)
+            {
+                var font = new RenderFont("Arial", 14);
+                var text = $"Total candle volume={candle.Volume}";
+                var textSize = context.MeasureString(text, font);
+                var textRectangle = new Rectangle(MouseLocationInfo.LastPosition.X + 10, MouseLocationInfo.LastPosition.Y + 10, (int)textSize.Width, (int)textSize.Height);
+
+                context.FillRectangle(Color.CornflowerBlue, textRectangle);
+                context.DrawString(text, font, Color.AliceBlue, textRectangle);
+            }
         }
 
         [Display(GroupName = "Variables", Name = "Period", Order = 10)]
